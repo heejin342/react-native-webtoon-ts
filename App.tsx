6ea7854d94c88445efae4457da35ae79dec94a62
heejin342 <tktmzp0526@naver.com>
2020-10-09 17:47:50 +0900

    git 정리

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer, RouteProp} from '@react-navigation/native';
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import Home from './screen/Home';
+import Search from './screen/Search';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+Ionicons.loadFont();
+const Tab = createBottomTabNavigator();
+import {mainColor} from './utils/color';
+
+const App = () => {
+  const screenOptions = ({
+    route,
+  }: {
+    route: RouteProp<Record<string, object | undefined>, string>;
+    navigation: any;
+  }): BottomTabNavigationOptions => ({
+    tabBarIcon: ({focused, color, size}) => {
+      let iconName: string = '';
+
+      if (route.name === '웹툰') {
+        iconName = focused
+          ? 'chatbubble-ellipses'
+          : 'chatbubble-ellipses-outline';
+      } else if (route.name === '검색') {
+        iconName = 'search';
+      }
+
+      // You can return any component that you like here!
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+  });
+
+  const tabBarOption = {
+    activeTintColor: mainColor,
+    inactiveTintColor: 'gray',
+  };
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions} tabBarOptions={tabBarOption}>
+        <Tab.Screen name="웹툰" component={Home} />
+        <Tab.Screen name="검색" component={Search} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ utils/color.ts
@@ -0,0 +1,1 @@
+export const mainColor = '#BDBF0A';
